3e929e50f1ef5c1420f9cfd72d0c802774ea6615
Sebastian Björkqvist <sebastian@sebastianbjorkqvist.com>
2017-03-30 20:34:18 +0300

    last-element done

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -7,8 +7,15 @@
                    (recur (* acc base) base (dec exp))))]
     (helper 1 base exp)))
 
+(defn singleton? [coll]
+  (and (not (empty? coll)) (empty? (rest coll))))
+
 (defn last-element [a-seq]
-  ":(")
+  (let [helper (fn [a-seq]
+                (if (or (empty? a-seq) (singleton? a-seq))
+                  (first a-seq)
+                  (recur (rest a-seq))))]
+  (helper a-seq)))
 
 (defn seq= [seq1 seq2]
   ":(")
